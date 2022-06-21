7e773242731fb5bf025eed7258bbc40e0f112911
Aleksandra Ujvari <ujvari65@gmail.com>
2022-06-21 21:49:56 +0200

    Declare constant.

---
+++ script.js
@@ -0,0 +1,6 @@
+const homepage = document.querySelector('.homepage');
+const menulist = document.querySelector('.list');
+const hamburger = document.querySelector('.hamburger');
+const closeIcon = document.querySelector('.menu-closebtn .times');
+const menuItems = Array.from(document.querySelectorAll('.item'));
+
